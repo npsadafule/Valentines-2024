656ffaec9bb45c09f89609b6d631740c0c974860
Neel Sadafule <neel.sadafule13@gmail.com>
2024-02-10 00:48:21 -0800

    Added styling to message component

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,15 @@
-// Message.js
 import React from 'react';
+import './Message.css'; // Import the CSS file here
 
 function Message() {
   return (
-    <div>
-      <h2>Yayyy!!!😘</h2>
-      <img src="https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif" alt="Love Gif" />
+    <div className="message-container">
+      <h2 className="love-message">Yayyy!!!😘</h2>
+      <img 
+        src="https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif" 
+        alt="Love Gif" 
+        className="love-gif"
+      />
     </div>
   );
 }
